90e951552166dacc4952a991dbf3355395d04e67
Rajat <rajatpoddar17@gmail.com>
2026-02-24 22:36:07 +0530

    bash file directory update

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# 1. Apne Cabelwala project folder me jao (Agar NAS pe path alag hai to isko change kar lena)
-cd /volume1/docker/cabelwala/
+# 1. Cabelwala Project folder me jao
+cd /volume1/docker/Projects/Cabelwala/
 
 # --- FIX START ---
 # Git permission error fix karne ke liye ye line jodi hai
-git config --global --add safe.directory /volume1/docker/cabelwala
+git config --global --add safe.directory /volume1/docker/Projects/Cabelwala
 # --- FIX END ---
 
 # 2. GitHub se naya code khicho
